825b7e3093746d32ca1441c87e150ecd59f60491
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-09-24 16:25:42 -0400

    context store

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from "react";
+// import { useSelector } from "react-redux";
 
-import ProductItem from '../components/Products/ProductItem';
-import './Products.css';
+import ProductItem from "../components/Products/ProductItem";
+import "./Products.css";
 
-const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+import { ProductsContext } from "../context/ProductsContext";
+
+const Products = (props) => {
+  const productList = useContext(ProductsContext).products;
+  // const productList = useSelector(state => state.shop.products);
   return (
     <ul className="products-list">
-      {productList.map(prod => (
+      {productList.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
